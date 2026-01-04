facd31dd22c896fdb17e28740ded4ad8a765c15a
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-04 20:08:03 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -9,8 +9,14 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        exisiting_user = self.user_repo.get_by_attribute("email", user_data["email"])
+        if exisiting_user:
+            raise ValueError("Email already exists")
+
+        user = User(**user_data)
+        self.user_repo.add(user)
+
+    return user
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
