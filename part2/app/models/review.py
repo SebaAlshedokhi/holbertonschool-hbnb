9237c5f738a6151d09ba648b0ae3173734bded47
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-04 19:44:27 +0300

    Update review.py

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -5,11 +5,16 @@ class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
 
+        if not text:
+            raise ValueError("Text required")
+        if rating < 1 or rating > 5:
+            raise ValueError("Rating must be between 1 and 5")
+        if not isinstance(place, Place):
+            raise TypeError("place must be a valid Place instance")
+        if not isinstance(user, User):
+            raise TypeError("user must be a valid User instance")
+            
         self.text = text
         self.rating = rating
         self.place = place
         self.user = user
-
-    def add_review(self, text):
-        """Add a text to the review."""
-        self.text.append(text)
