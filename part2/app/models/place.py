1369c5ea5f0634fd9d24cbf20743bd3aa9124a4d
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-04 19:14:30 +0300

    Update place.py

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -4,6 +4,18 @@ from app.models.BaseEntity import BaseModel
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
+
+        if not title or len(title) > 100:
+            raise ValueError("Invalid name")
+        if not price or price < 0:
+            raise ValueError("Invalid name")
+        if not latitude or latitude < -90.0 or latitude > 90.0:
+            raise ValueError("Invalid number")
+        if not longitude  or longitude  < -180.0 or longitude  > 180.0:
+            raise ValueError("Invalid number")
+        if not owner:
+            raise ValueError("User does not exist")
+
         self.title = title
         self.description = description
         self.price = price
