e10431cfe6712c47a0cf45974c6ee057c9a7b0b0
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-06 18:44:01 +0300

    Update users.py

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -28,7 +28,7 @@ class UserList(Resource):
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
-        def get(self):
+    def get(self):
         """Retrieve all users"""
         users = facade.get_all_users()  # ← تحتاج هذه الدالة في facade
         users_list = [
