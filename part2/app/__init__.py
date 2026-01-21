25e2778a6db7ceb8d7ea1cdbeb85931de2654745
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-21 18:36:21 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,36 +1,26 @@
 from flask import Flask
 from flask_restx import Api
 
-from app.api.v1.places import api as places_ns
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
 
-
-def create_app(config_class="config.DevelopmentConfig"): #update to receive a configuration
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
-----------------------
-    from yourapplication.model import db
-    db.init_app(app)
-
-    from yourapplication.views.admin import admin
-    from yourapplication.views.frontend import frontend
-    app.register_blueprint(admin)
-    app.register_blueprint(frontend)
------------------------
-    
     api = Api(
         app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/'
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/",
     )
 
-    # Register namespaces
+    # Import and register namespaces
+    from hbnb.app.api.v1.users import api as users_ns
+    from hbnb.app.api.v1.amenities import api as amenities_ns
+    from hbnb.app.api.v1.places import api as places_ns
+    from hbnb.app.api.v1.reviews import api as reviews_ns
+
     api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
